8a5a5cbaa1d881eafdae227706424d5e125c1bd6
dona6382 <46666529+dona6382@users.noreply.github.com>
2023-01-17 09:01:29 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,52 @@
 const express = require('express');
 const path = require('path');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const session = require('express-session'); // 요청마다 개인의 저장공간 생성
 
 const app = express();
 
 app.set('port', process.env.PORT || 3000);
 
 
-app.use('/about',(req, res, next) => {
-  console.log('1 요청을 실행')
-  next();
-},(req, res, next) => {
-  throw new Error('에러가 났어요');
+app.use(morgan('dev'));
+
+app.use(cookieParser('hwipassword'));
+app.use(session({
+  resave : false,
+  saveUninitialized : false,
+  secret : 'hwipassword',
+  cookie : {
+    httpOnly : true,
+  },
+}));
+app.use('/', (req, res, next) => {
+  if(req.session.id){
+    express.static(__dirname, 'public')(req, res, next) //뒤에 req, res, net 붙이면 확장성 추가
+  }else {
+    next();
+  }
 });
+app.use(express.json());
+//
+// app.use((req, res, next) => {
+//   console.log('1 요청을 실행')
+//   next();
+// },(req, res, next) => {
+//   try{
+//   }catch (error){
+//     next(error);
+//   }
+// });
+
+
+app.use((req, res,next) => {
+  req.data = '비밀번호';
+})
 
 app.get('/', (req, res) => {
+  //req.session.id = 'hello';
+  req.data
   res.sendFile(path.join(__dirname, '/views/index.html'));
 });
 
